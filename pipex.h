47963599e5927be72a8f09feef8c43642b4f47f6
Lorenzo Yanga <lorenzoyanga@gmail.com>
2025-09-30 07:25:28 +0800

    remove unneeded

--- pipex.h
+++ pipex.h
@@ -6,20 +6,18 @@
 /*   By: lyanga <lyanga@student.42singapore.sg>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/27 13:22:43 by lyanga            #+#    #+#             */
-/*   Updated: 2025/09/30 07:21:10 by lyanga           ###   ########.fr       */
+/*   Updated: 2025/09/30 07:25:16 by lyanga           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PIPEX_H
 # define PIPEX_H
 
-# include <unistd.h>     // fork, pipe, dup2, execve, access, close, read, write
-# include <stdlib.h>     // malloc, free, exit
-# include <stdio.h>      // perror
-# include <fcntl.h>      // open
-# include <sys/wait.h>   // wait, waitpid
-# include <string.h>     // strerror (if needed)
-# include <stdarg.h>    
+# include <unistd.h>
+# include <stdlib.h>
+# include <stdio.h>
+# include <fcntl.h>
+# include <sys/wait.h> 
 
 # include "libft.h"
 
